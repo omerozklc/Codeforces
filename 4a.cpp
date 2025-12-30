9f2a6609f122b8d79908e24b3581d56853baf473
omerozklc <ozklcomer@gmail.com>
2025-12-31 00:27:17 +0300

    Watermelon

---
+++ 4a.cpp
@@ -0,0 +1,21 @@
+//
+// Omer Ozkilic 12/31/2025 00:15
+//
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void solve() {
+	int input;
+	cin >> input;
+	if(input % 2 != 0 || input == 2) cout << "NO";
+	else cout << "YES";
+}	
+
+int main() {
+	ios::sync_with_stdio(false);
+    	cin.tie(0);
+    	solve();
+    	return 0;
+}

538850f6277c53f00515f4bde3be8de91728a653
omerozklc <ozklcomer@gmail.com>
2025-12-31 03:34:27 +0300

    Domino Piling

---
+++ 50a.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+void solve() {
+  int m, n;
+  cin >> m >> n;
+  cout << (m * n / 2);
+}	
+
+int main() {
+	ios::sync_with_stdio(false);
+  cin.tie(0);
+  solve();
+  return 0;
+}
